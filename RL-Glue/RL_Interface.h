7badf9868a1626afbc177dc5f3c13e102f4b5bb4
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-04-14 01:55:31 +0000

    Working on the socket-agent

--- RL-Glue/RL_Interface.h
+++ RL-Glue/RL_Interface.h
@@ -1,22 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef RL_Interface_h
+#define RL_Interface_h
+
 #include "RLcommon.h"
 
-#ifndef RL_INTERFACE_H
-#define RL_INTERFACE_H
-
 typedef struct {
-	Reward r;
-	Observation o;
-	Action a;
-	bool terminal;
+  Reward r;
+  Observation o;
+  Action a;
+  int terminal;
 } Reward_observation_action_terminal;
 
 typedef struct {
-	Observation o;
-	Action a;
+  Observation o;
+  Action a;
 } Observation_action;
 
+/* Glue */
 void RL_init();
 Observation_action RL_start();
 Reward_observation_action_terminal RL_step();
@@ -34,17 +33,20 @@ State_key RL_get_state();
 Random_seed_key RL_get_random_seed();
 void RL_cleanup();
 
-void agent_init(Task_specification task_);
+/* Agent Interface */
+void agent_init(Task_specification task_spec);
 Action agent_start(Observation o);
 Action agent_step(Reward r, Observation o);
 void agent_end(Reward r);  
 void agent_cleanup();
 
+/* Environment Interface */
 Task_specification env_init();
 Observation env_start();
 Reward_observation env_step(Action a);
 void env_cleanup();
 
+/* State Key Interface */
 void env_set_state(State_key sk);
 void env_set_random_seed(Random_seed_key rsk);
 State_key env_get_state();
