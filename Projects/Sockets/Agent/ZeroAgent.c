37bf4cda739d5d5da77c82218642fdf18d68bfca
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-04-18 00:47:38 +0000

    Adding Glue-to-Agent Interface

--- Projects/Sockets/Agent/ZeroAgent.c
+++ Projects/Sockets/Agent/ZeroAgent.c
@@ -7,7 +7,7 @@ Observation theObservation;
 void agent_init(Task_specification task_spec)
 {
   theAction.numInts = 1;
-  theAction.intArray = (int*)malloc(sizeof(int) * theAction.numInts);
+  theAction.intArray = (int*)calloc(theAction.numInts, sizeof(int));
 }
 
 Action agent_start(Observation o)
@@ -30,4 +30,5 @@ void agent_end(Reward r)
 
 void agent_cleanup()
 {
+  free (theAction.intArray);
 }
