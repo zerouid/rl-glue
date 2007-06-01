fb4b3ce8253e704f272ab38e2d4398012840604a
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-01 19:42:28 +0000

    Added network code

---
+++ RL-Glue/Network/RL_network_agent.c
@@ -0,0 +1,10 @@
+#include <Network/RLnet.h>
+
+static int agent_connection = 0;
+
+void rlSetAgentConnection(int connection) {
+  if (agent_connection)
+    rlClose(agent_connection);
+  
+  agent_connection = connection;
+}
