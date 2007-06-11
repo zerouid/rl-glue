f474b9f58c1fedfe3aaacbce55de74d9b0a823fb
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-11 19:37:48 +0000

    Updated some stuff.

--- RL-Glue/Network/Glue/RL_server_experiment.c
+++ RL-Glue/Network/Glue/RL_server_experiment.c
@@ -18,108 +18,149 @@ extern State_key RL_get_state();
 extern Random_seed_key RL_get_random_seed();
 extern void RL_cleanup();
 
+State_key theStateKey = {0};
+Random_seed_key theRandomSeedKey = {0};
+int isStateKeyAllocated = 0;
+int isRandomSeedKeyAllocated = 0;
+
 void onRLInit(rlSocket theConnection) {
-	RL_init();
+  RL_init();
 }
 
 void onRLStart(rlSocket theConnection) {
-	RL_start();
+  Observation_action obsAct = RL_start();
+
+  rlSendADT(theConnection, &obsAct.o);
+  rlSendADT(theConnection, &obsAct.a);
 }
 
 void onRLStep(rlSocket theConnection) {
-	RL_step();
+  Reward_observation_action_terminal roat = RL_step();
+  
+  rlSendData(theConnection, &roat.r, sizeof(Reward));
+  rlSendADT(theConnection, &roat.o);
+  rlSendADT(theConnection, &roat.a);
+  rlSendData(theConnection, &roat.terminal, sizeof(int));
 }
 
 void onRLReturn(rlSocket theConnection) {
-	RL_return();
+  Reward theReward = RL_return();
+  rlSendData(theConnection, &theReward, sizeof(int));
 }
 
 void onRLNumSteps(rlSocket theConnection) {
-	RL_num_steps();
+  int numSteps = RL_num_steps();
+  rlSendData(theConnection, &numSteps, sizeof(int));
 }
 
 void onRLNumEpisodes(rlSocket theConnection) {
-	RL_num_episodes();
+  int numEpisodes = RL_num_episodes();
+  rlSendData(theConnection, &numEpisodes, sizeof(int));
 }
 
 void onRLEpisode(rlSocket theConnection) {
-	RL_episode();
+  int numEpisodes = 0;
+  
+  rlRecvData(theConnection, &numEpisodes, sizeof(int));
+  RL_episode(numEpisodes);
 }
 
 void onRLSetState(rlSocket theConnection) {
-	RL_set_state();
+  rlRecvADTHeader(theConnection, &theStateKey);
+  if (!isStateKeyAllocated) {
+    rlAllocADT(&theStateKey);
+    isStateKeyAllocated = 1;
+  }
+  rlRecvADTBody(theConnection, &theStateKey);
+  
+  RL_set_state(theStateKey);
 }
 
-void onRLSetRandomSeed(rlSocket theConnection) {
-	RL_set_random_seed();
+void onRLSetRandomSeed(rlSocket theConnection) {	
+  rlRecvADTHeader(theConnection, &theRandomSeedKey);
+  if (!isRandomSeedKeyAllocated) {
+    rlAllocADT(&theRandomSeedKey);
+    isRandomSeedKeyAllocated = 1;
+  }
+  rlRecvADTBody(theConnection, &theRandomSeedKey);
+  
+  RL_set_random_seed(theRandomSeedKey);
 }
 
 void onRLGetState(rlSocket theConnection) {
-	RL_get_state();
+  State_key theStateKey = RL_get_state();
+  rlSendADT(theConnection, &theStateKey);
 }
 
 void onRLGetRandomSeed(rlSocket theConnection) {
-	RL_get_random_seed();
+  Random_seed_key theRandomSeedKey = RL_get_random_seed();
+  rlSendADT(theConnection, &theRandomSeedKey);
 }
 
 void onRLCleanup(rlSocket theConnection) {
-	RL_cleanup();
+  RL_cleanup();
+  
+  rlFreeADT(&theStateKey);
+  rlFreeADT(&theRandomSeedKey);
+  
+  isStateKeyAllocated      = 0;
+  isRandomSeedKeyAllocated = 0;
 }
 
 void runGlueEventLoop(rlSocket theConnection) {
   int glueState = 0;
   do { 
     rlRecvData(theConnection, &glueState, sizeof(int));
-
+    
     switch(glueState) {
     case kRLInit:
       onRLInit(theConnection);
       break;
-
+      
     case kRLStart:
       onRLStart(theConnection);
       break;
-
+      
     case kRLStep:
       onRLStep(theConnection);
       break;
-
+      
     case kRLReturn:
       onRLReturn(theConnection);
       break;
-
+      
     case kRLCleanup:
       onRLCleanup(theConnection);
       break;
-
+      
     case kRLNumSteps:
       onRLNumSteps(theConnection);
       break;
-
+      
     case kRLNumEpisodes:
       onRLNumEpisodes(theConnection);
       break;
-
+      
     case kRLEpisode:
       onRLEpisode(theConnection);
       break;
-
+      
     case kRLSetState:
       onRLSetState(theConnection);
       break;
-
+      
     case kRLSetRandomSeed:
       onRLSetRandomSeed(theConnection);
       break;
-
+      
     case kRLGetState:
       onRLGetState(theConnection);
       break;
-
+      
     case kRLGetRandomSeed:
       onRLGetRandomSeed(theConnection);
       break;
-
+      
     default:
       fprintf(stderr, kUnknownMessage, glueState);
       break;
@@ -127,12 +168,10 @@ void runGlueEventLoop(rlSocket theConnection) {
   } while (glueState != kRLCleanup);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   while(1) {
-	rlConnectSystems();
+    rlConnectSystems();
     runGlueEventLoop();
   }
-
   return 0;
 }
