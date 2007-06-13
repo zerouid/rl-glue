05abe3a57508bcf4a557d9ad08b18a7bcb254cc8
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-13 20:17:20 +0000

    Agent freeze implemented

--- Projects/Network/Agent/ZeroAgent.c
+++ Projects/Network/Agent/ZeroAgent.c
@@ -5,30 +5,28 @@
 Action theAction;
 Observation theObservation;
 
-void agent_init(Task_specification task_spec)
-{
+void agent_init(Task_specification task_spec) {
   theAction.numInts = 1;
   theAction.intArray = (int*)malloc(sizeof(int) * theAction.numInts);
 }
 
-Action agent_start(Observation o)
-{
+Action agent_start(Observation o) {
   theObservation = o;
   theAction.intArray[0] = rand();
   return theAction;
 }
 
-Action agent_step(Reward r, Observation o)
-{
+Action agent_step(Reward r, Observation o) {
   theObservation = o;
   theAction.intArray[0] = rand();
   return theAction;
 }
 
-void agent_end(Reward r)
-{
+void agent_end(Reward r) {
 }
 
-void agent_cleanup()
-{
+void agent_cleanup() {
+}
+
+void agent_freeze() {
 }
--- Projects/Network/Agent/ZeroAgent.h
+++ Projects/Network/Agent/ZeroAgent.h
@@ -8,5 +8,6 @@ Action agent_start(Observation o);
 Action agent_step(Reward r, Observation o);
 void agent_end(Reward r);
 void agent_cleanup();
+void agent_freeze();
 
 #endif
