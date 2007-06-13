f84ea2395fa9d68ae902a5be95cd6511ce90ab98
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-13 19:57:27 +0000

    Added RL_freeze and agent_freeze

--- branches/ISSUE-3/RL-Glue/RL_interface.h
+++ branches/ISSUE-3/RL-Glue/RL_interface.h
@@ -7,17 +7,18 @@
 void RL_init();
 Observation_action RL_start();
 Reward_observation_action_terminal RL_step();
+void RL_cleanup();
+
+void RL_episode(unsigned int num_steps);
 Reward RL_return();
 int RL_num_steps();
 int RL_num_episodes();
-
 /* void RL_episode(); */
-void RL_episode(unsigned int num_steps);
+void RL_freeze();
 void RL_set_state(State_key sk);
 void RL_set_random_seed(Random_seed_key rsk);
 State_key RL_get_state();
 Random_seed_key RL_get_random_seed();
-void RL_cleanup();
 
 /* Agent Interface */
 void agent_init(Task_specification task_spec);
@@ -25,6 +26,7 @@ Action agent_start(Observation o);
 Action agent_step(Reward r, Observation o);
 void agent_end(Reward r);  
 void agent_cleanup();
+void agent_freeze();
 
 /* Environment Interface */
 Task_specification env_init();
