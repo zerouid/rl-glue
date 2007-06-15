49d821c5b81c2510f447a285243d138cc8a8c916
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-15 06:48:02 +0000

    ensuring the agent gets the right things.

--- Projects/Network/Agent/ZeroAgent.c
+++ Projects/Network/Agent/ZeroAgent.c
@@ -5,6 +5,20 @@
 Action theAction;
 Observation theObservation;
 
+void agent_print(const char* header, RL_abstract_type* data) {
+  int i = 0;
+  fprintf(stderr, "%s", header);
+  fprintf(stderr, "%d %d\n", data->numInts, data->numDoubles);
+
+  for (i = 0; i < data->numInts; ++i)
+    fprintf(stderr, "%d ", data->intArray[i]);
+  fprintf(stderr, "\n");
+
+  for (i = 0; i < data->numDoubles; ++i)
+    fprintf(stderr, "%f ", data->doubleArray[i]);
+  fprintf(stderr, "\n");
+}
+
 void agent_init(Task_specification task_spec) {
   theAction.numInts  = 1;
   theAction.intArray = (int*)malloc(sizeof(int) * theAction.numInts);
@@ -16,12 +30,20 @@ void agent_init(Task_specification task_spec) {
 Action agent_start(Observation o) {
   theObservation = o;
   theAction.intArray[0] = rand();
+
+  agent_print("agent_start observation ", &theObservation);
+  agent_print("agent_start action ", &theAction);
+
   return theAction;
 }
 
 Action agent_step(Reward r, Observation o) {
   theObservation = o;
   theAction.intArray[0] = rand();
+
+  agent_print("agent_step observation ", &theObservation);
+  agent_print("agent_step action ", &theAction);
+
   return theAction;
 }
 
