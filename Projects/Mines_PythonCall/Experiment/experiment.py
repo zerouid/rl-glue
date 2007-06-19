a555dbd3bc2f8b3423e64fa4969954337880e037
markplee <markplee@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-19 22:14:37 +0000

    Created meaningful experiments for all 3 projects

--- Projects/Mines_PythonCall/Experiment/experiment.py
+++ Projects/Mines_PythonCall/Experiment/experiment.py
@@ -3,12 +3,30 @@ import sys
 sys.path = sys.path + ['./Build']
 from RL_client_experiment import *
 
+NUM_EPISODES = 100
+rl_num_steps = []
+rl_return = []
+
 def run(num_episodes):
   for x in range(num_episodes):
-    RL_episode(10)
+    RL_episode(0)
     sys.stderr.write(".")
+    rl_num_steps.append(RL_num_steps())
+    rl_return.append(RL_return())
 
 
 RL_init()
-run(100)
+run(NUM_EPISODES)
 RL_cleanup()
+avg_steps = 0.0
+avg_return = 0.0
+for i in range(NUM_EPISODES):
+	avg_steps += rl_num_steps[i]
+	avg_return += rl_return[i]
+avg_steps /= NUM_EPISODES
+avg_return /= NUM_EPISODES
+print "\n-----------------------------------------------"
+print "Number of episodes:",NUM_EPISODES
+print "Average number of steps per episode:",avg_steps
+print "Average return per episode:", avg_return
+print "-----------------------------------------------"
