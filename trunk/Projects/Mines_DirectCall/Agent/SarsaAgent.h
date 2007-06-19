a26bf12a5ae88644052246e0d9d8b43a6851d466
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-19 23:46:24 +0000

    fixing rl-glue to also compile with G++

--- trunk/Projects/Mines_DirectCall/Agent/SarsaAgent.h
+++ trunk/Projects/Mines_DirectCall/Agent/SarsaAgent.h
@@ -8,6 +8,7 @@ Action agent_start(Observation o);
 Action agent_step(Reward r, Observation o);
 void agent_end(Reward r);
 void agent_cleanup();
+Message agent_message(const Message message);
 int egreedy(Observation o);
 
 #endif
