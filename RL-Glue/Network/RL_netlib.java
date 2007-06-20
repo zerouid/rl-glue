f8ec34466b187af5e025cb68560e1b05dfdf881d
markplee <markplee@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-20 22:57:54 +0000

    Java!

--- RL-Glue/Network/RL_netlib.java
+++ RL-Glue/Network/RL_netlib.java
@@ -115,4 +115,24 @@ public class RL_netlib
 				}
 			}
 		}
+		
+		public RLAbstractType readAbstractType() {
+			numInts = inputStream.readInt();
+			numDoubles = inputStream.readInt();
+			int[] intArray;
+			double[] doubleArray;
+			if (numInts > 0) {
+				intArray = new int[numInts];
+				for (int i = 0; i < numInts; i++) {
+					intArray[i] = inputStream.readInt();
+				}
+			}
+			if (numDoubles > 0) {
+				doubleArray = new double[numDoubles];
+				for (int i = 0; i < numDoubles; i++) {
+					doubleArray[i] = inputStream.readDouble();
+				}
+			}
+			return new RLAbstractType(numInts,numDoubles,intArray,doubleArray);
+		}
 }
