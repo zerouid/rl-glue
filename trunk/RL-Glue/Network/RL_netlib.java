8eaba5e5fc2b33a951dd865aa202507418de256d
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-20 19:51:35 +0000

    added the appropriate constants to RL_netlib.java

--- trunk/RL-Glue/Network/RL_netlib.java
+++ trunk/RL-Glue/Network/RL_netlib.java
@@ -10,7 +10,51 @@ public class RL_netlib
     protected ByteBuffer byteBuffer;
     protected DataInputStream inputStream;
     protected DataOutputStream outputStream;
-    protected BufferedReader stdin;
+
+    public static final int kExperimentConnection = 1;
+    public static final int kAgentConnection = 2;
+    public static final int kEnvironmentConnection = 3;
+
+    public static final int kAgentInit = 4;
+    public static final int kAgentStart = 5;
+    public static final int kAgentStep = 6;
+    public static final int kAgentEnd = 7;
+    public static final int kAgentCleanup = 8;
+    public static final int kAgentFreeze = 30;
+    public static final int kAgentMessage = 31;
+
+    public static final int kEnvInit = 9;
+    public static final int kEnvStart = 10;
+    public static final int kEnvStep = 11;
+    public static final int kEnvCleanup = 12;
+    public static final int kEnvSetState = 13;
+    public static final int kEnvSetRandomSeed = 14;
+    public static final int kEnvGetState = 15;
+    public static final int kEnvGetRandomSeed = 16;
+    public static final int kEnvMessage = 32;
+
+    public static final int kRLInit = 17;
+    public static final int kRLStart = 18;
+    public static final int kRLStep = 19;
+ 
+
+    public static final int kRLCleanup = 20;
+    public static final int kRLReturn = 21;
+    public static final int kRLNumSteps = 22;
+    public static final int kRLNumEpisodes = 23;
+    public static final int kRLEpisode = 24;
+    public static final int kRLSetState = 25;
+    public static final int kRLSetRandomSeed = 26;
+    public static final int kRLGetState = 27;
+    public static final int kRLGetRandomSeed = 28;
+    public static final int kRLFreeze = 29;
+    public static final int kRLAgentMessage = 33;
+    public static final int kRLEnvMessage = 34;
+
+    public static final String kLocalHost = "127.0.0.1";
+    public static final int kDefaultPort = 4096;
+    public static final int kRetryTimeout = 10;
+
 
     public RL_netlib(String host, int port) throws Exception
     {
