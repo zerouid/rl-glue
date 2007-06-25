c6f1f2ccf4d60e2e68d119b763221ced8f6ff53f
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-25 23:31:56 +0000

    Removed redundant RL_network.c

--- RL-Glue/Network/RL_netlib.c
+++ RL-Glue/Network/RL_netlib.c
@@ -2,8 +2,7 @@
 #include <assert.h> /* assert */
 #include <stdlib.h> /* calloc */
 #include <string.h> /* memset */
-
-/*#include <stdio.h>*/ /* fprintf : Debug only */
+#include <stdio.h> /* fprintf */
 
 /* Network Headers */
 #include <unistd.h>
@@ -339,4 +338,87 @@ void rlCopyBufferToADT(const rlBuffer* src, RL_abstract_type* dst) {
   if (dst->numDoubles > 0) {
     offset = rlBufferRead(src, offset, dst->doubleArray, dst->numDoubles, sizeof(double));
   }
+}
+
+
+/* Below: Was RL_network.c  Reorganization allowed us to remove a source file */
+
+rlSocket theAgentConnection = -1;
+rlSocket theEnvironmentConnection = -1;
+rlSocket theExperimentConnection = -1;
+
+/* 
+   rlConnectSystems is responsible for ensuring that the Glue has an agent, environment and experiment
+   program before the Glue attempts to run an experiment.
+
+   If the experiment and the Glue are compiled together RL_init calls rlConnectSystems.
+   If the Glue is separate, it has its own main() that calls rlConnectSystems.   
+*/
+
+int rlConnectSystems() {
+  int isAgentConnected       = (theAgentConnection != 0);
+  int isEnvironmentConnected = (theEnvironmentConnection != 0); 
+  int isExperimentConnected  = (theExperimentConnection != 0);
+  int theClientType = 0;
+  int theClient = 0;
+  int theServer = 0;
+  rlBuffer theBuffer = {0};
+  short port = kDefaultPort;
+  char* envptr = 0;
+
+  /* fprintf(stderr, "isAgentConnected = %d, isEnvConnected = %d, isExpConnected = %d\n",isAgentConnected, isEnvironmentConnected, isExperimentConnected); */
+
+  rlBufferCreate(&theBuffer, sizeof(int) * 2);
+
+  if (!isAgentConnected || !isEnvironmentConnected || !isExperimentConnected) {
+    envptr = getenv("RLGLUE_PORT");  
+    if (envptr != 0) {
+      port = strtol(envptr, 0, 10);
+      if (port == 0) {
+	port = kDefaultPort;
+      }
+      fprintf(stderr, "RL-Glue is listening for connections on port=%d\n", port);
+    }
+
+    theServer = rlOpen(port);
+    rlListen(theServer, port);
+  }
+
+  while(!isAgentConnected || !isEnvironmentConnected || !isExperimentConnected) {
+    theClient = rlAcceptConnection(theServer);
+
+    /* fprintf(stderr, "%s %d: Connection Attempt\n", __FILE__, __LINE__); */
+
+    rlRecvBufferData(theClient, &theBuffer);
+    rlBufferRead(&theBuffer, 0, &theClientType, 1, sizeof(int));
+
+    switch(theClientType) {
+    case kAgentConnection:
+      /* fprintf(stderr, "agent connected!\n"); */
+      theAgentConnection = theClient;
+      isAgentConnected = 1;
+      break;
+
+    case kEnvironmentConnection:
+      /* fprintf(stderr, "env connected\n"); */
+      theEnvironmentConnection = theClient;
+      isEnvironmentConnected = 1;
+      break;
+
+    case kExperimentConnection:
+      /* fprintf(stderr, "exp connected\n"); */
+      isExperimentConnected = 1;
+      theExperimentConnection = theClient;
+      break;
+
+    default:
+      fprintf(stderr, "RL_network.c: Unknown Connection Type: %d\n", theClientType);
+      break;
+    };
+  }
+
+  rlClose(theServer);
+  rlBufferDestroy(&theBuffer);
+
+  return theExperimentConnection;
 }
