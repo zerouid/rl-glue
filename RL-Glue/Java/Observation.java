52396733410662dcf3b4c8a3b9aeada56468e298
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-25 21:17:27 +0000

    Moved RL_netlib.java

--- RL-Glue/Java/Observation.java
+++ RL-Glue/Java/Observation.java
@@ -1,5 +1,9 @@
-public class Observation extends RLAbstractType {
+public class Observation {
+    public int [] intArray;
+    public double [] doubleArray;
+
     public Observation(int numInts, int numDoubles) {
-	super(numInts, numDoubles);
+	intArray = new int[numInts];
+	doubleArray = new double[numDoubles];
     }
 }
