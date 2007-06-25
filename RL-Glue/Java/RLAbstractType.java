d25518aa5b5afe0ad66ac898150a463162fc509e
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-25 21:23:32 +0000

    removed rlabstracttype.java

--- RL-Glue/Java/RLAbstractType.java
+++
@@ -1,9 +0,0 @@
-public class RLAbstractType {
-    public int [] intArray;
-    public double [] doubleArray;
-
-    public RLAbstractType(int numInts, int numDoubles) {
-	intArray = new int[numInts];
-	doubleArray = new double[numDoubles];
-    }
-}
