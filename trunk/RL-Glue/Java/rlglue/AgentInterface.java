3721c67ff12f9c5c6a8550ce091480b2a35b2a50
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-26 19:53:30 +0000

    Updating Agent stuff

--- trunk/RL-Glue/Java/rlglue/AgentInterface.java
+++ trunk/RL-Glue/Java/rlglue/AgentInterface.java
@@ -2,4 +2,11 @@ package rlglue;
 
 public interface AgentInterface 
 {
+    void agent_init(final String taskSpecification);
+    Action agent_start(Observation observation);
+    Action agent_step(double reward, Observation observation);
+    void agent_end(double reward);
+    void agent_cleanup();
+    void agent_freeze();
+    String agent_message(final String message);
 }
