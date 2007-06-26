4c2434c122010323dc37f4653c058ed439da09d2
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-26 22:12:44 +0000

    Commiting changes

---
+++ trunk/Examples/Network_Java/src/SarsaAgent.h
@@ -0,0 +1,14 @@
+#ifndef SarsaAgent_h
+#define SarsaAgent_h
+
+#include <RL_common.h>
+
+void agent_init(const Task_specification task_spec);
+Action agent_start(Observation o);
+Action agent_step(Reward r, Observation o);
+void agent_end(Reward r);
+void agent_cleanup();
+Message agent_message(const Message msg);
+int egreedy(Observation o);
+
+#endif
