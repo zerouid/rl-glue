8307d361a615d6e231e2c5f4cecd242a92e9fe06
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-06-26 19:53:30 +0000

    Updating Agent stuff

--- RL-Glue/Java/rlglue/EnvironmentInterface.java
+++ RL-Glue/Java/rlglue/EnvironmentInterface.java
@@ -2,4 +2,13 @@ package rlglue;
 
 public interface EnvironmentInterface
 {
+    String env_init();
+    Observation env_start();
+    Reward_observation env_step();
+    void env_cleanup();
+    void env_set_state(State_key key);
+    void env_set_random_seed(Random_seed_key key);
+    State_key env_get_state();
+    Random_seed_key env_get_random_seed();
+    String env_message(final String message);
 }
