7172fd15d07a8dd34151ce941492762b3c2dd002
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-12 20:04:51 +0000

    Updated AgentMain to look at environment variables

--- trunk/RL-Glue/Java/Agent/AgentMain.java
+++ trunk/RL-Glue/Java/Agent/AgentMain.java
@@ -6,15 +6,35 @@ public class AgentMain
     {
 	Agent agent = (Agent)Class.forName(args[0]).newInstance();
 	ClientAgent client = new ClientAgent(agent);
-	boolean autoReconnect = false;
+	int autoReconnect = 0;
 
 	String host = Network.kDefaultHost;
 	int port = Network.kDefaultPort;
+
+	String hostString = System.getenv("RLGLUE_HOST");
+	String portString = System.getenv("RLGLUE_PORT");
+	String reconnect = System.getenv("RLGLUE_AUTORECONNECT");
+
+	host = hostString;
+
+	try {
+	    port = Integer.parseInt(portString);
+	}
+	catch (Exception e) {
+	    port = Network.kDefaultPort;
+	}
+
+	try {
+	    autoReconnect = Integer.parseInt(reconnect);
+	}
+	catch (Exception e) {
+	    autoReconnect = 0;
+        }
 
 	do {
 	    client.connect(host, port, Network.kRetryTimeout);
 	    client.runAgentEventLoop();
 	    client.close();
-	} while (autoReconnect);
+	} while (autoReconnect == 1);
     }
 }
