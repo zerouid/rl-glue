dd6a199371a6af469114fefd79e22ebb8705841a
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-12 20:21:11 +0000

    Forgot to add parts of the Java code to the repo

---
+++ trunk/RL-Glue/Java/rlglue/Reward_observation.java
@@ -0,0 +1,8 @@
+package rlglue;
+
+public class Reward_observation 
+{
+    public double r;
+    public Observation o;
+    public int terminal;
+}
---
+++ trunk/RL-Glue/Java/rlglue/Reward_observation_action_terminal.java
@@ -0,0 +1,13 @@
+package rlglue;
+
+public class Reward_observation_action_terminal
+{
+    public double r;
+    public Observation o;
+    public Action a;
+    public int terminal;
+
+    public Reward_observation_action_terminal()
+    {
+    }
+}
