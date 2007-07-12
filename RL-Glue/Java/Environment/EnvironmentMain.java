8d17601cc2d350d8a83447667dfa573648a09053
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-12 20:10:03 +0000

    Updated environment main to get and print out environment variables

--- RL-Glue/Java/Environment/EnvironmentMain.java
+++ RL-Glue/Java/Environment/EnvironmentMain.java
@@ -11,6 +11,28 @@ public class EnvironmentMain
 	String host = Network.kDefaultHost;
 	int port = Network.kDefaultPort;
 
+	String hostString = System.getenv("RLGLUE_HOST");
+	String portString = System.getenv("RLGLUE_PORT");
+	String reconnect = System.getenv("RLGLUE_AUTORECONNECT");
+
+	host = hostString;
+
+	try {
+	    port = Integer.parseInt(portString);
+	}
+	catch (Exception e) {
+	    port = Network.kDefaultPort;
+	}
+
+	try {
+	    autoReconnect = Integer.parseInt(reconnect);
+	}
+	catch (Exception e) {
+	    autoReconnect = 0;
+        }
+
+	System.out.println("Connecting to " + host + " on port " + port + " with autoreconnect=" + autoReconnect);
+
 	do {
 	    client.connect(host, port, Network.kRetryTimeout);
 	    client.runEnvironmentEventLoop();
