677197755625d433ba438ad709a4926df2c882e3
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-17 19:07:44 +0000

    Added constructors to compound data types in java

--- RL-Glue/Java/rlglue/Reward_observation_action_terminal.java
+++ RL-Glue/Java/rlglue/Reward_observation_action_terminal.java
@@ -10,4 +10,12 @@ public class Reward_observation_action_terminal
     public Reward_observation_action_terminal()
     {
     }
+
+    public Reward_observation_action_terminal(double reward, Observation observation, Action action, int terminal)
+    {
+	this.r = reward;
+	this.o = observation;
+	this.a = action;
+	this.terminal = terminal;
+    }
 }
