d4d88888a056a5b084eab8fd5b25e641fa5eac08
burnt.rain <burnt.rain@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-19 20:10:26 +0000

    "added the discrete and continuous counts in the parser for java"

--- Utils/TaskSpecParser.java
+++ Utils/TaskSpecParser.java
@@ -10,6 +10,8 @@ public class TaskSpecParser
 	String [] tmp = null;
 	String [] tmp2 = null;
 	String [] tmp3 = null;
+	int discrete =0;
+	int continuous =0;
 
 	try 
 	{
@@ -27,15 +29,25 @@ public class TaskSpecParser
         
 	    tmp3 = tmp2[1].split(",");
 	    
-	    if(tsObject.obs_dim == 1)
+	    if(tsObject.obs_dim == 1){
 		tsObject.obs_types[0] = tmp3[0].substring(1,tmp3[0].length()-1).trim().charAt(0);
+		if(tsObject.obs_types[0] == 'i')
+			discrete++;
+		else
+			continuous++;
+		}
 	    else
 	    {
 		tmp3[0] = tmp3[0].substring(1,tmp3[0].length()); 
 		tmp3[tsObject.obs_dim-1] = tmp3[tsObject.obs_dim-1].substring(0, tmp3[tsObject.obs_dim-1].length()-1);
 		
-		for(int i = 0; i < tsObject.obs_dim; i++)
+		for(int i = 0; i < tsObject.obs_dim; i++){
 		    tsObject.obs_types[i] = (tmp3[i].trim()).charAt(0);
+			if(tsObject.obs_types[i] == 'i')
+				discrete++;
+			else
+				continuous++;
+		}
 	    }
 
 	    for(int i = 2; i < tsObject.obs_dim + 2; i++)
@@ -47,6 +59,11 @@ public class TaskSpecParser
 		tsObject.obs_mins[i-2] = Float.parseFloat(tmp3[0]);
 		tsObject.obs_maxs[i-2] = Float.parseFloat(tmp3[1]);
 	    }
+		
+		tsObject.num_discrete_obs_dims = discrete;
+		tsObject.num_continuous_obs_dims = continuous;
+		discrete =0;
+		continuous =0;
     
 	    tmp2 = null;
 	    tmp2 = tmp[3].split("_");
@@ -58,17 +75,27 @@ public class TaskSpecParser
 	    
 	    tmp3 = tmp2[1].split(",");
 	    
-	    if(tsObject.action_dim == 1)
+	    if(tsObject.action_dim == 1){
 		tsObject.action_types[0] = tmp3[0].substring(1,tmp3[0].length()-1).trim().charAt(0);        
+		if(tsObject.obs_types[0] == 'i')
+			discrete++;
+		else
+			continuous++;
+		}
 	    else
 	    {   
 		tmp3[0] = tmp3[0].substring(1,tmp3[0].length()); 
 		tmp3[tsObject.action_dim-1] = tmp3[tsObject.action_dim-1].substring(0, tmp3[tsObject.action_dim-1].length()-1);
 		
-		for(int i = 0; i < tsObject.action_dim; ++i)
+		for(int i = 0; i < tsObject.action_dim; ++i){
 		    tsObject.action_types[i] = (tmp3[i].trim()).charAt(0);
+			if(tsObject.obs_types[0] == 'i')
+				discrete++;
+			else
+				continuous++;
 	    }
-	    
+	    }
+		
 	    for(int i = 2; i < tsObject.action_dim + 2; ++i)
 	    {
 		tmp3 = null;
@@ -79,6 +106,10 @@ public class TaskSpecParser
 		tsObject.action_maxs[i-2] = Float.parseFloat(tmp3[1]);
 	    }
 	    
+		tsObject.num_discrete_action_dims = discrete;
+		tsObject.num_continuous_action_dims = continuous;
+		discrete =0;
+		continuous =0;
 	}
 	catch (Exception e)
 	{
