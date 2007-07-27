65be71dca046038f5a9d838e1c20bfa1f8e85e2e
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-27 17:38:18 +0000

    Just corrected the source-indentation for eclipse, but I dont want it to think I'm out of date with the repo based on that, so I'm committing.

--- trunk/RL-Glue/Java/src/rlglue/types/Action.java
+++ trunk/RL-Glue/Java/src/rlglue/types/Action.java
@@ -2,16 +2,16 @@ package rlglue.types;
 
 public class Action 
 {
-    public int [] intArray;
-    public double [] doubleArray;
+	public int [] intArray;
+	public double [] doubleArray;
 
-    public Action() 
-    {
-    }
+	public Action() 
+	{
+	}
 
-    public Action(int numInts, int numDoubles)
-    {
-	intArray = new int[numInts];
-	doubleArray = new double[numDoubles];
-    }
+	public Action(int numInts, int numDoubles)
+	{
+		intArray = new int[numInts];
+		doubleArray = new double[numDoubles];
+	}
 }
--- trunk/RL-Glue/Java/src/rlglue/types/Reward_observation.java
+++ trunk/RL-Glue/Java/src/rlglue/types/Reward_observation.java
@@ -2,18 +2,18 @@ package rlglue.types;
 
 public class Reward_observation 
 {
-    public double r;
-    public Observation o;
-    public int terminal;
+	public double r;
+	public Observation o;
+	public int terminal;
 
-    public Reward_observation()
-    {
-    }
+	public Reward_observation()
+	{
+	}
 
-    public Reward_observation(double reward, Observation observation, int terminal)
-    {
-	this.r = reward;
-	this.o = observation;
-	this.terminal = terminal;
-    }
+	public Reward_observation(double reward, Observation observation, int terminal)
+	{
+		this.r = reward;
+		this.o = observation;
+		this.terminal = terminal;
+	}
 }
