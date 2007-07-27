f5db8dcfa60e8febd760c133b37277c098d0f9eb
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-07-27 16:31:04 +0000

    Initial import.  Adding working RL-Glue java back in.  With eclipse!

---
+++ RL-Glue/Java/src/rlglue/Observation_action.java
@@ -0,0 +1,18 @@
+package rlglue;
+
+public class Observation_action
+{
+    public Observation o;
+    public Action a;
+
+    public Observation_action()
+    {
+    }
+
+    public Observation_action(Observation theObservation, Action theAction)
+    {
+	o = theObservation;
+	a = theAction;
+    }
+
+}
