21495b9c1e6ea8ebaf72207b2e78903773012801
andrewbutcher <andrewbutcher@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-08-15 01:40:53 +0000

    Debugging RL_env_message some more. the problem seems to be in cloneWithCapacity

--- RL-Glue/Java/src/rlglue/network/Network.java
+++ RL-Glue/Java/src/rlglue/network/Network.java
@@ -58,7 +58,7 @@ public class Network
 	public static final int kDefaultPort = 4096;
 	public static final int kRetryTimeout = 10;
 
-	protected static final int kByteBufferDefaultSize = 65536;
+	protected static final int kByteBufferDefaultSize = 65536*2*2*2;
 	public static final int kIntSize = 4;
 	protected static final int kDoubleSize = 8;
 	
