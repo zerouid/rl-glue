61718681a1ea22f62ae93a3bc2deba2b4b2bd9d1
markplee <markplee@e37465e7-f52a-0410-81ad-11548b01ce4d>
2007-10-30 00:46:27 +0000

    Removed Tests folder

--- Tests/Network_Java/bin/runExample.bash
+++
@@ -1,5 +0,0 @@
-./RL_Glue &
-
-java -cp ~/workspace/Java/bin:./ rlglue.agent.AgentLoader SarsaAgent &
-java -cp ~/workspace/Java/bin:./ rlglue.environment.EnvironmentLoader Mines &
-java -cp ~/workspace/Java/bin:./ Experiment
