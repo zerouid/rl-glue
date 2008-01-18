013ef031ecd45b85ddd3cf4a7ca860e5a9804b9f
radkie <radkie@e37465e7-f52a-0410-81ad-11548b01ce4d>
2008-01-18 19:31:46 +0000

    changed rlglue.h

--- trunk/RL-Glue/RL_glue.h
+++ trunk/RL-Glue/RL_glue.h
@@ -19,6 +19,8 @@
 #define RL_interface_h
 
 #include <RL_common.h>
+#include <Environment_common.h>
+#include <Agent_common.h>
 
 /* Glue */
 void RL_init();
@@ -39,25 +41,5 @@ void RL_set_state(State_key sk);
 void RL_set_random_seed(Random_seed_key rsk);
 State_key RL_get_state();
 Random_seed_key RL_get_random_seed();
-
-/* Agent Interface */
-void agent_init(const Task_specification task_spec);
-Action agent_start(Observation o);
-Action agent_step(Reward r, Observation o);
-void agent_end(Reward r);  
-void agent_cleanup();
-void agent_freeze();
-Message agent_message(const Message message);
-
-/* Environment Interface */
-Task_specification env_init();
-Observation env_start();
-Reward_observation env_step(Action a);
-void env_cleanup();
-void env_set_state(State_key sk);
-void env_set_random_seed(Random_seed_key rsk);
-State_key env_get_state();
-Random_seed_key env_get_random_seed();
-Message env_message(const Message message);
 
 #endif
