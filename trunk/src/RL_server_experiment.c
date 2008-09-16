f9a3b93111e9b7e987d742f3b7174c553768fdf8
brian@tannerpages.com <brian@tannerpages.com@e37465e7-f52a-0410-81ad-11548b01ce4d>
2008-09-16 20:21:40 +0000

    Added a newline to RL_debug.c and -no-undefined to LD_FLAGS so that it woudl build on Windows (windows needs all symbols defined a compile time)

--- trunk/src/RL_server_experiment.c
+++ trunk/src/RL_server_experiment.c
@@ -401,7 +401,7 @@ int main(int argc, char** argv) {
 	fprintf(stdout, "RL-Glue Version %s, Build %s\n", VERSION,SVN_VERSION);
 	fflush(stdout);
   
-  snprintf(usageBuffer,1024,"\n\trl_glue version\t=\t%s\n\tbuild number\t=\t%s\n\nUsage: $:>rl_glue\n\n  By default rl_glue listens on port 4096.\n  To choose a different port, set environment variable RLGLUE_PORT.\n\n",VERSION,SVN_VERSION);
+  sprintf(usageBuffer,"\n\trl_glue version\t=\t%s\n\tbuild number\t=\t%s\n\nUsage: $:>rl_glue\n\n  By default rl_glue listens on port 4096.\n  To choose a different port, set environment variable RLGLUE_PORT.\n\n",VERSION,SVN_VERSION);
 
   if (argc > 1) {
     fprintf(stdout, usageBuffer);
