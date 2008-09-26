462b17ac4c50fc0cca2aed231e4e6a494abec89e
whitienator <whitienator@e37465e7-f52a-0410-81ad-11548b01ce4d>
2008-09-26 21:22:10 +0000

    trying out with docs in svn

---
+++ trunk/docs/Glue-Docs/labels.pl
@@ -0,0 +1,33 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/exp/;
+$external_labels{$key} = "$URL/" . q|node37.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/env/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ref/;
+$external_labels{$key} = "$URL/" . q|node49.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/agent/;
+$external_labels{$key} = "$URL/" . q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/quick/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
