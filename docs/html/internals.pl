c81dd3bdc7721278655f8ac02676998f414e8fa6
brian <brian@tannerpages.com>
2008-09-29 01:49:37 +0000

    Added a technical manual for direct-compile experiments.

---
+++ docs/html/internals.pl
@@ -0,0 +1,26 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/exp/;
+$ref_files{$key} = "$dir".q|Glue-Docs.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/env/;
+$ref_files{$key} = "$dir".q|Glue-Docs.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ref/;
+$ref_files{$key} = "$dir".q|Glue-Docs.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/agent/;
+$ref_files{$key} = "$dir".q|Glue-Docs.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/quick/;
+$ref_files{$key} = "$dir".q|Glue-Docs.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
