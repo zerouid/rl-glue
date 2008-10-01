97b1b08458de5ed34f07876d20494cf272047e8f
whitienator <whitienator@e37465e7-f52a-0410-81ad-11548b01ce4d>
2008-10-01 01:58:39 +0000

    adding to the docs

--- trunk/docs/Glue-Docs/images.pl
+++ trunk/docs/Glue-Docs/images.pl
@@ -5,43 +5,37 @@
 $key = q/gamma;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="14" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img5.png"
+ SRC="|."$dir".q|img3.png"
  ALT="$\gamma$">|; 
 
 $key = q/<;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="18" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img6.png"
+ SRC="|."$dir".q|img4.png"
  ALT="$&lt;$">|; 
 
 $key = q/rightarrow;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="22" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
+ SRC="|."$dir".q|img1.png"
  ALT="$\rightarrow$">|; 
 
 $key = q/geq;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="18" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img8.png"
+ SRC="|."$dir".q|img6.png"
  ALT="$\geq$">|; 
 
 $key = q/alpha;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="15" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img4.png"
+ SRC="|."$dir".q|img2.png"
  ALT="$\alpha$">|; 
-
-$key = q/``;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$\lq\lq $">|; 
 
 $key = q/neq;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="18" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img7.png"
+ SRC="|."$dir".q|img5.png"
  ALT="$\neq$">|; 
 
 1;
--- trunk/docs/Glue-Docs/internals.pl
+++ trunk/docs/Glue-Docs/internals.pl
@@ -2,24 +2,72 @@
 # Associate internals original text with physical files.
 
 
+$key = q/glos/;
+$ref_files{$key} = "$dir".q|node36.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/agentp1/;
+$ref_files{$key} = "$dir".q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/RL-Glue/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/exp/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/faq/;
+$ref_files{$key} = "$dir".q|node35.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/env/;
-$ref_files{$key} = "$dir".q|node9.html|; 
+$ref_files{$key} = "$dir".q|node7.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/ref/;
-$ref_files{$key} = "$dir".q|node49.html|; 
+$ref_files{$key} = "$dir".q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Eref/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/envp1/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig1/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/task/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/envp2/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/expp1/;
+$ref_files{$key} = "$dir".q|node29.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/agent/;
-$ref_files{$key} = "$dir".q|node24.html|; 
+$ref_files{$key} = "$dir".q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/expp2/;
+$ref_files{$key} = "$dir".q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/quick/;
 $ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/agentp2/;
+$ref_files{$key} = "$dir".q|node25.html|; 
 $noresave{$key} = "$nosave";
 
 1;
