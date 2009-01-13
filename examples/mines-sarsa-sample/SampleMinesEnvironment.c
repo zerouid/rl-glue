7b71a0fffd30eb8a3601b980dda09f1c3effa83b
brian <brian@tannerpages.com>
2009-01-13 23:52:19 +0000

    First draft of the new sample project, and first draft of Scott Livingston's task spec parser.

--- examples/mines-sarsa-sample/SampleMinesEnvironment.c
+++ examples/mines-sarsa-sample/SampleMinesEnvironment.c
@@ -1,43 +1,135 @@
 /* 
-* Copyright (C) 2008, Brian Tanner
+	Copyright (C) 2008, Brian Tanner
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+	    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+	This code is adapted from the Mines.cpp code written by Mark Lee
+	for earlier versions of RL-Glue.
+	
+	*  $Revision: 821 $
+	*  $Date: 2008-09-14 14:21:41 -0600 (Sun, 14 Sep 2008) $
+	*  $Author: brian@tannerpages.com $
+	*  $HeadURL: https://rl-glue.googlecode.com/svn/trunk/src/RL_direct_agent.c $
+	
+*/
 
 #include <string.h> /*strcmp*/
 #include <stdio.h> /*printf*/
 #include <stdlib.h>
+#include <assert.h> /*assert*/
 
-#include <rlglue/Environment_common.h>/* env_ function prototypes and RL-Glue types */
-#include <rlglue/utils/C/RLStruct_util.h> /* helpful functions for allocating structs and cleaning them up */
+#include <rlglue/Environment_common.h>	  /* env_ function prototypes types */
+#include <rlglue/utils/C/RLStruct_util.h> /* helpful functions for allocating
+ 											  structs and cleaning them up */
 
-/* This is a very simple environment with discrete observations corresponding to states labeled {0,1,...,19,20}
-The starting state is 10.
-
-There are 2 actions = {0,1}.  0 decrements the state, 1 increments the state.
-
-The problem is episodic, ending when state 0 or 20 is reached, giving reward -1 or +1, respectively.  The reward is 0 on 
-all other steps.
+/* 	
+	This is a very simple discrete-state, episodic grid world that has 
+	exploding mines in it.  If the agent steps on a mine, the episode
+	ends with a large negative reward.
+	
+	The reward per step is -1, with +10 for exiting the game successfully
+	and -100 for stepping on a mine.
 */
-  
-observation_t this_observation;
-reward_observation_terminal_t this_reward_observation;
-int current_state=0;
 
 
-const char* env_init()
-{    
-	char* task_spec="2:e:1_[i]_[0,20]:1_[i]_[0,1]:[-1,1]";
+
+
+#define WORLD_FREE 0
+#define WORLD_OBSTACLE 1
+#define WORLD_MINE 2 
+#define WORLD_GOAL 3
+
+/*
+	world_description_t is a structure that holds all of the information
+	about the world, including the placement of the mines and the position
+	of the agent, etc.
+*/
+typedef struct 
+{
+  int numRows;		/* Number of rows in the world		*/
+  int numCols;		/* Number of columns in the world	*/
+  int agentRow;		/* Agent current row				*/
+  int agentCol;		/* Agent current column				*/
+} world_description_t;
+
+
+/* HELPER FUNCTIONS.  PROTOTYPES HERE, CODE AT THE BOTTOM OF FILE */
+
+/* Changes the (row,col) position of the agent into a scalar     */	
+int calculate_flat_state(world_description_t aWorld);
+
+
+/* Returns 1 of the current state is terminal (mine or goal), 0 otherwise */
+int check_terminal(int row, int col);
+
+
+/* Returns 1 of the current state is inside map and not blocked) */
+int check_valid(const world_description_t* aWorld,int row, int col);
+
+/*  Calculates the reward in the current state.
+	-100 if on a mine
+	+10 if at the exit
+	-1 otherwise 
+*/
+double calculate_reward(world_description_t aWorld);
+
+/* Calculates and sets the next state, given an action */
+void updatePosition(world_description_t *aWorld, int theAction);
+
+
+/*
+	world_map is an array that describes the world.
+
+	To read this: the world is a 6 by 18 grid in any position the number 
+	corresponds to one of {START, GOAL, FREE, OBSTACLE, MINE}
+
+	For example in env_init the start position is labelled by a 0
+	so we can see the initial start position in this particular map is at
+	position [12][1]
+	
+*/
+
+int world_map[6][18] = 
+{ 
+	{ 1, 1, 1, 1, 1, 1 ,1 ,1, 1, 1 ,1 ,1 ,1, 1, 1, 1, 1, 1 }, 
+	{ 1, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, 
+	{ 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 1, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 1, 1, 1, 1, 1 ,1 ,1, 1, 1 ,1 ,1 ,1, 1, 1, 1, 1, 1 }
+};
+
+
+/* GLOBAL VARIABLES FOR RL-GLUE methods (global for convenience) */  
+static world_description_t the_world;
+static observation_t this_observation;
+static reward_observation_terminal_t this_reward_observation;
+
+
+/*****************************
+
+	RL-Glue Methods 
+	
+*******************************/
+const char* env_init(){    
+	char *task_spec_string="VERSION RL-Glue-3.0 PROBLEMTYPE episodic "
+							 "DISCOUNTFACTOR 1 OBSERVATIONS INTS (0 107) "
+							 "ACTIONS INTS (0 3)  REWARDS (-100.0 10.0) "
+							"EXTRA SampleMinesEnvironment by Brian Tanner.";
+
+	  the_world.numRows = 6;
+	  the_world.numCols = 18;
+
 
 	/* Allocate the observation variable */
 	allocateRLStruct(&this_observation,1,0,0);
@@ -54,40 +146,44 @@ const char* env_init()
 	this_reward_observation.reward=0;
 	this_reward_observation.terminal=0;
 
-   return task_spec;
+   return task_spec_string;
 }
 
+
+/*
+	Standard RL-Glue method. Sets an initial state and returns
+	the corresponding observation.
+*/
 const observation_t *env_start()
 { 
-	current_state=10;
-	this_observation.intArray[0]=current_state;
+	int startRow=rand()%6;
+	int startCol=rand()%18;
+	
+	while(check_terminal(startRow,startCol) || !check_valid(&the_world,startRow,startCol)){
+		startRow=rand()%6;
+		startCol=rand()%18;
+	}
+	
+	the_world.agentRow=startRow;
+	the_world.agentCol=startCol;
+
+	this_observation.intArray[0]=calculate_flat_state(the_world);
   	return &this_observation;
 }
 
+
 const reward_observation_terminal_t *env_step(const action_t *this_action)
 {
-	int episode_over=0;
-	double the_reward=0;
-	
-	if(this_action->intArray[0]==0)
-		current_state--;
-	if(this_action->intArray[0]==1)
-		current_state++;
+	/* Make sure the action is valid */
+	assert(this_action->numInts==1);
+	assert(this_action->intArray[0]>=0);
+	assert(this_action->intArray[0]<4);
 
-	if(current_state<=0){
-		current_state=0;
-		episode_over=1;
-		the_reward=-1;
-	}
-	if(current_state>=20){
-		current_state=20;
-		episode_over=1;
-		the_reward=1;
-	}
+	updatePosition(&the_world,this_action->intArray[0]);
+	this_reward_observation.observation->intArray[0] = calculate_flat_state(the_world);
+	this_reward_observation.reward = calculate_reward(the_world);
+	this_reward_observation.terminal = check_terminal(the_world.agentRow,the_world.agentCol);
 
-	this_reward_observation.observation->intArray[0] = current_state;
-	this_reward_observation.reward = the_reward;
-	this_reward_observation.terminal = episode_over;
 
 	return &this_reward_observation;
 }
@@ -98,8 +194,73 @@ void env_cleanup()
 }
 
 const char* env_message(const char* inMessage) {
-	if(strcmp(inMessage,"what is your name?")==0)
-		return "my name is skeleton_environment!";
-
-	return "I don't know how to respond to your message";
+	return "SamplesMinesEnvironment does not respond to any messages.";
 }
+
+
+/*****************************
+
+	Helper Methods 
+	
+*******************************/
+
+int calculate_flat_state(world_description_t aWorld){
+	return aWorld.agentCol * aWorld.numRows + aWorld.agentRow;
+}
+
+int check_terminal(int row, int col){
+	if (world_map[row][col] == WORLD_GOAL || world_map[row][col] == WORLD_MINE){    
+		return 1;
+	}
+	return 0;
+}
+
+int check_valid(const world_description_t* aWorld, int row, int col){
+	int valid=0;
+	if(row < aWorld->numRows && row >= 0 && col < aWorld->numCols && col >= 0){
+		if(world_map[row][col] != WORLD_OBSTACLE){
+			valid=1;
+		}
+	}
+	return valid;
+}
+
+double calculate_reward(world_description_t aWorld){
+	if(world_map[aWorld.agentRow][aWorld.agentCol] == WORLD_GOAL){
+		return 10.0f;
+	}
+
+	if(world_map[aWorld.agentRow][aWorld.agentCol] == WORLD_MINE){
+		return -100.0f;
+	}
+	
+	return -1.0f;
+}
+
+void updatePosition(world_description_t *aWorld, int theAction){
+	/* When the move would result in hitting an obstacles, the agent simply doesn't move */
+	int newRow = aWorld->agentRow;
+	int newCol = aWorld->agentCol;
+
+	
+	if (theAction == 0){/*move down*/
+		newCol = aWorld->agentCol - 1;
+	}
+	if (theAction == 1){ /*move up*/
+		newCol = aWorld->agentCol + 1;
+	}
+	if (theAction == 2){/*move left*/
+		newRow = aWorld->agentRow - 1;
+	}
+	if (theAction == 3){/*move right*/
+		newRow = aWorld->agentRow + 1;
+	}
+
+
+	/*Check if new position is out of bounds or inside an obstacle */
+	if(check_valid(aWorld,newRow,newCol)){
+   		aWorld->agentRow = newRow;
+   		aWorld->agentCol = newCol;
+	}
+}
+
